a135565e90492199a6eee5957fe404f1bf76c9c0
undina <irina_suponeva@relex.ru>
2024-01-29 11:23:36 +0300

    add rabbitmq

--- dispatcher/src/main/java/ru/undina/service/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/ru/undina/service/impl/UpdateProducerImpl.java
@@ -1,15 +1,21 @@
 package ru.undina.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.undina.service.UpdateProducer;
 
 @Service
 @Log4j
+@RequiredArgsConstructor
 public class UpdateProducerImpl implements UpdateProducer {
+    private final RabbitTemplate rabbitTemplate;
+
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
